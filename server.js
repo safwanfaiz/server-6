db15cdbae161978ae103414693a329a0fd3dfa31
SafwanFaiz <safwanfaiz8@gmail.com>
2023-03-21 12:52:56 +0530

    json

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; // <== You can change the port
-
+const cors = require('cors');
+server.use(cors({
+    origin: '*'
+}));
 server.use(middlewares);
 server.use(router);
 
